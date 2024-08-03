4339cf104b9f75ff6a9e6a6d520ba469f0fff6f3
H3R01A <aallen0001@gmail.com>
2024-08-03 16:27:33 -0700

    update actions.ts for implementing functions to get bitcoin data

--- actions/actions.ts
+++ actions/actions.ts
@@ -1,11 +1,48 @@
 'use server';
-
+import 'server-only';
+import xss from 'xss';
 import { redirect } from 'next/navigation';
+import { revalidatePath } from 'next/cache';
+
+export async function getBicoinBlockData() {
+  const response = await fetch(`https://blockchain.info/q/getblockcount`);
+  revalidatePath('/');
+  return response.json();
+}
+
+export async function getBitcoinPriceData() {
+  const headers: HeadersInit = {
+    'x-cg-demo-api-key': process.env.COINGECKO_API_KEY ?? '',
+  };
+
+  const response = await fetch(
+    `https://api.coingecko.com/api/v3/coins/bitcoin`,
+    {
+      headers,
+    }
+  );
+
+  revalidatePath('/');
+  return response.json();
+}
 
 export async function handleTickerSubmission(formData: FormData) {
-  const tickerInput = formData.get('ticker');
+  let tickerInput = formData.get('ticker');
   const userWallet = formData.get('address');
-  redirect(`/token/${tickerInput}?address=${userWallet}`);
+
+  if (typeof tickerInput !== 'string') {
+    throw new Error('Invalid ticker input');
+  }
+
+  const sanitizedTickerInput = xss(tickerInput);
+
+  if (typeof userWallet !== 'string') {
+    throw new Error('Invalid user wallet input');
+  }
+
+  const sanitizeduserWallet = xss(userWallet);
+
+  redirect(`/token/${sanitizedTickerInput}?address=${sanitizeduserWallet}`);
 }
 
 export async function handleOrderSubmission(formData: FormData) {
@@ -15,7 +52,7 @@ export async function handleOrderSubmission(formData: FormData) {
 
 export async function getTickerData(ticker: String) {
   const headers: HeadersInit = {
-    'x-api-key': process.env.API_KEY ?? '',
+    'x-api-key': process.env.OB_API_KEY ?? '',
   };
 
   const response = await fetch(
@@ -34,7 +71,7 @@ export async function getTickerData(ticker: String) {
 
 export async function getTickerBalance(address: String, ticker: String) {
   const headers: HeadersInit = {
-    'x-api-key': process.env.API_KEY ?? '',
+    'x-api-key': process.env.OB_API_KEY ?? '',
   };
   const response = await fetch(
     `https://api.ordinalsbot.com/opi/v1/brc20/get_current_balance_of_wallet?address=bc1pxaneaf3w4d27hl2y93fuft2xk6m4u3wc4rafevc6slgd7f5tq2dqyfgy06&ticker=${ticker}`,
