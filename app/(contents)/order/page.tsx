d793b3d57b8c4d163fcd2d43a8c6a4eeb76b294c
H3R01A <aallen0001@gmail.com>
2024-08-03 13:14:32 -0700

    update order and token pages with submit form

--- app/(contents)/order/page.tsx
+++ app/(contents)/order/page.tsx
@@ -1,13 +1,14 @@
 import '../../globals.css';
+import { handleOrderSubmission } from '@/actions/actions';
 
 export default function OrderPage() {
 
 
   return (
     <>
-    <h1>Enter your Order Number Below to Check the Status</h1>
-    <form>
-        <input />
+    <h1>Enter Order ID below to check the status of your order</h1>
+    <form action={handleOrderSubmission}>
+        <input type='text' name='order' />
         <button>Check</button>
       </form>
     </>
--- app/(contents)/token/page.tsx
+++ app/(contents)/token/page.tsx
@@ -1,11 +1,12 @@
 import '../../globals.css';
+import { handleTickerSubmission } from '@/actions/actions';
 
 export default function TokenLandingPage() {
   return (
     <>
     <h1>Enter a BRC20 Token Below</h1>
-      <form>
-        <input />
+      <form action={handleTickerSubmission}>
+        <input type='text' name='ticker' />
         <button>Check</button>
       </form>
     </>
